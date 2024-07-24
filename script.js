6d665ebd02f6354d0dd8b1d251e76605abaa4d52
brannycoders <nathanielbn@gmail.com>
2024-07-24 09:47:30 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// Strings
+var name = "John Doe";
+var city = "New York";
+var hobby = "Photography";
+var language = "JavaScript";
+var food = "Pizza";
+
+// Numbers
+var age = 30;
+var temperature = 98.6;
+var score = 85;
+var distance = 100;
+var year = 2024;
+
+// Booleans
+var isStudent = true;
+var hasLicense = false;
+var isAdmin = true;
+var isActive = false;
+var isOnline = true;
+
+// Null
+var job = null;
+var car = null;
+var address = null;
+var phone = null;
+var email = null;
+
+// Undefined
+var country;
+var state;
+var city;
+var street;
+var zipcode;
